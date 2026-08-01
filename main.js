37dc02c5ccdf3ffb8b107c6c9d40af9cfb4dcb7b
Mahliyo <gasandigital13@gmail.com>
2026-08-01 22:28:10 +0500

    href

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
 
 
-var ism = "Malika";
-console.log(ism);
+// var ism = "Malika";
+// console.log(ism);
+
+
+import React from 'react'
+
+function main() {
+  return (
+    <div>main</div>
+  )
+}
+
+export default main
